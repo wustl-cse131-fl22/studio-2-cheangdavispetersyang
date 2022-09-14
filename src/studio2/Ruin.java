af69b4baa9f8ab135748abbf22977ac68e03a9e6
student <student@DESKTOP-9Q5Q0AV>
2022-09-14 15:48:09 -0500

    Made it!

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -9,7 +9,7 @@ public class Ruin {
 		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter the starting amount:");
-		int startAmount = in.nextInt();
+		int setAmount = in.nextInt();
 		
 		System.out.println("Enter your win limit:");
 		int winLimit = in.nextInt();
@@ -17,21 +17,45 @@ public class Ruin {
 		System.out.println("Enter your win probability (between 0-1):");
 		double winChance = in.nextDouble();
 		
+		System.out.println("Enter the total number of simulations:");
+		int totalSimulations = in.nextInt();
+		
+		int currentSimulations = 0;
+		int accRuin = 0;
+		
+		while (currentSimulations < totalSimulations)
+		{
+			int startAmount = setAmount; 
+			int numPlays = 0;
 		while (startAmount >0 && winLimit != startAmount)
 		{
 			double gamble = (Math.random());
 				if (gamble > winChance)
 				{ 
-					System.out.println("ruin!");
+					//System.out.println("ruin!");
 					startAmount--;
 				}
 				else
 				{
-					System.out.println("success!");
+					//System.out.println("success!");
 					startAmount++;
 				}
+				numPlays++;
+				
 		}
+				
+				currentSimulations++;		
+				System.out.println("Day: " + currentSimulations);
+				System.out.println("Finishing amount: " + startAmount);
+				System.out.println("Number of Plays: " + numPlays);
 			
+				if (startAmount == 0)
+				{
+				accRuin++;	
+				}
+		}	
+		double ruinRate = accRuin / totalSimulations;
+		System.out.println("Ruin Rate: " + ruinRate);
 	}
 
 }
